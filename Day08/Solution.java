399e3e71661a7b6200fd90c88313bedad82d0299
Keerthiya T <keerthithirumurthy1@gmail.com>
2025-04-25 21:54:47 +0530

    Create Solution.java

---
+++ Day08/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maximumProfit(int prices[]) {
+        int minimum=prices[0];
+        int n=prices.length;
+        int res=0;
+        for(int i=1;i<n;i++){
+            minimum=Math.min(minimum,prices[i]);
+            res=Math.max(res,prices[i]-minimum);
+        }
+        return res;
+    }
+}
