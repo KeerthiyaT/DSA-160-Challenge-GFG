cae5bd82850991e520dbcb7c338b90869b4ae37d
Keerthiya T <keerthithirumurthy1@gmail.com>
2025-04-26 21:16:45 +0530

    Update Solution.java
    rotate arrays by d steps left

--- Day04/Solution.java
+++ Day04/Solution.java
@@ -7,7 +7,7 @@ class Solution {
         reverse(arr,0,d-1);
         reverse(arr,d,n-1);
         reverse(arr,0,n-1);
-        
+     
     }
     static void reverse(int arr[],int s,int e){
         while(s<e){
