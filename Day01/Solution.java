571e661f91a46f28956bd758cfb7d58cf0c08a4d
Keerthiya T <keerthithirumurthy1@gmail.com>
2025-04-26 21:18:33 +0530

    Update Solution.java
    Second largest element

--- Day01/Solution.java
+++ Day01/Solution.java
@@ -2,7 +2,7 @@ class Solution {
     public int getSecondLargest(int[] arr) {
         
         Arrays.sort(arr);
-        
+    
         int n=arr.length;
         for(int i=n-2;i>=0;i--){
             if(arr[n-1]!=arr[i]){
