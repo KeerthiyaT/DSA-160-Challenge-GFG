a9daf839d77160f9b657e33ae50a4b0d306b4090
Keerthiya T <keerthithirumurthy1@gmail.com>
2025-04-26 21:11:20 +0530

    Update Solution.java
    Minimize the heights II

--- Day09/Solution.java
+++ Day09/Solution.java
@@ -5,7 +5,7 @@ class Solution {
         Arrays.sort(arr);
         int res=arr[n-1]-arr[0];
         for(int i=0;i<n-1;i++){
-            
+        
                int min=Math.min(arr[0]+k,arr[i+1]-k);
                int max=Math.max(arr[n-1]-k,arr[i]+k);
                if(min<0) continue;
