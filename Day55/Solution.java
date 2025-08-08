5cc3dd0c9a777b95620b58e97f5f1c35423410ec
Keerthiya T <keerthithirumurthy1@gmail.com>
2025-08-08 21:34:20 +0530

    Create Solution.java
    Count the number of possible triangles
    Input: arr[] = [4, 6, 3, 7]
    Output: 3
    Explanation: There are three triangles possible [3, 4, 6], [4, 6, 7] and [3, 6, 7]. Note that [3, 4, 7] is not a possible triangle.

---
+++ Day55/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countTriangles(int arr[]) {
+        // code here
+        int n=arr.length;
+        Arrays.sort(arr);
+        int c=0;
+        for(int i=n-1;i>=2;i--){
+           int j=0,k=i-1;
+           while(j<k){
+           if(arr[j]+arr[k]>arr[i]){
+               c+=k-j;
+               k--;
+           }else{
+               j++;
+           }
+           }
+        }return c;
+    }
+}
